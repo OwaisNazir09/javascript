3bfe1660307b8d3f8c97b292dbdce058c8fc8436
OwaisNazir09 <owaisaalam38@gmail.com>
2024-01-24 22:43:21 +0530

    added data conversion

---
+++ Basics-of-javascript/data-conversion.js
@@ -0,0 +1,46 @@
+let age = 25
+
+// to get the type of this age  variable 
+// tehre are tho ways 
+
+//1 
+console.log(typeof age);
+
+// the 2nd way is using a function that is passing varaisable as an argument
+console.log(typeof(age))
+
+
+// conversion of s string into number
+
+let str = "owais"
+let name = String(str)
+
+console.log(typeof(name))
+//  now names -> string
+
+console.log(name )
+
+
+
+//conersion of string into  number 
+
+let s= 1
+let isnum = Number(s)
+
+console.log(typeof(isnum))
+//  isnum  -> number
+
+// suppose now s conatins numbers as well as letters 
+s = "264f";
+ isnum = Number(s)
+
+ console.log(typeof(isnum))
+ //  isnum  -> number
+
+ //check itss value 
+ console.log(isnum)
+ // now the output is nan i.e e not a number
+ 
+
+
+
