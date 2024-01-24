d573e17f63e93866d8da596053244ec7ece16784
OwaisNazir09 <owaisaalam38@gmail.com>
2024-01-24 23:12:56 +0530

    all about operators

---
+++ Basics-of-javascript/operators.js
@@ -0,0 +1,43 @@
+//(-)is used to make a number negative
+let i = 10
+let z = -i
+console.log(-i)
+
+//operators
+//add
+console.log(2+2)
+//SUBTRACT
+console.log(2-2)
+
+
+//MULTILICATION
+console.log(2*2)
+
+//DIVIDE
+console.log(43/23)
+
+//MODULES TO GET REMINDER
+console.log(28%2)
+
+//CONCATEATION(+) is used to join 2 strings
+let str1 = "owais ", str2 = " nazir"
+let str3 = str1 + str2;
+console.log(str3)
+
+
+//prefix and postfix increements
+let a = 234;
+
+a++
+console.log(a)
+
+
+
+
+
+
+let b = ++a
+
+// first increment it by 1 and then assign the value 
+console.log(a)
+console.log(b)
