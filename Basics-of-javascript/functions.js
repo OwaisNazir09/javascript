0d9734e016a65f644424676623e74849038b4a23
OwaisNazir09 <owaisaalam38@gmail.com>
2024-02-19 18:01:07 +0530

    learned about functios

---
+++ Basics-of-javascript/functions.js
@@ -0,0 +1,27 @@
+
+// A JavaScript function is a block of code designed to perform a particular task.
+
+// A JavaScript function is executed when "something" invokes it (calls it).
+//eg 
+
+function sayhello()
+{
+    console.log("helloo");
+
+}
+// this will not be prinetd we will call it
+// like
+sayhello();
+
+// lets make a function that takees arguemsts and performs some operation 
+function add(a,b)
+{
+    return a+b;
+}
+// here we saveed the output inside a variable
+console.log(add(45,34));
+let x = add(9,37);
+console.log(x);
+// thsi is how its donw without using an exttra variable
+
+console.log(add(45,34));
